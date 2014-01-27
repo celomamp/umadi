d58e971848390c703d264e3ccc6ea560870f0d68
filipesrosa <filipesrosa@gmail.com>
2014-01-27 11:16:54 -0200

    implementação menu

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
+	<meta charset="UTF-8">
+	<title>UMADI</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	
+
+<div class="menu">
+	<nav class="nav-menu">
+		<ul>
+			<li><a href="#">Home</a></li>
+		</ul>
+	</nav>
+</div>
+
 </body>
-</html>
