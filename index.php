342114112d5bb2730113f7dbd73f24afd476ae6f
celomamp <marcelo.pinheiro96@gmail.com>
2014-01-27 23:44:20 -0200

    implementação da tela inicial (login)

--- index.php
+++ index.php
@@ -6,20 +6,65 @@
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="rsc/css/bootstrap.min.css">
 	<script type="text/javascript" src="rsc/js/bootstrap.js"></script>
+	<script type="text/javascript" src="rsc/js/jquery.js"></script>
+	<script type="text/javascript" src="rsc/js/default.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Chivo:400,900' rel='stylesheet' type='text/css'>
+	<link href="rsc/themes/5/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="rsc/themes/5/js-image-slider.js" type="text/javascript"></script>
 </head>
 <body>
 
-<div class="menu">
-	<nav class="nav-menu">
-		<ul>
-			<li><a href="#">Home</a></li>
-		</ul>
-	</nav>
+
+<header>
+
+	<h1>UMADI</h1>
+	<p>UNIÃO DA MOCIDADE DA ASSEMBLÉIA DE DEUS DE ITAPIRA</p>
+	
+</header>
+
+<nav>
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Diretoria</a></li>
+		<li><a href="#">Agenda</a></li>
+		<li><a href="#">Recados</a></li>
+		<li><a href="#">Atividades</a></li>
+		<li><a href="#">Coral</a></li>
+		
+	</ul>
+
+</nav>
+
+<h1 class="sub">Venha Louvar Conosco!</h1>
+<div class="container">
+	
+	<div class="slide">
+	<?php
+		require('slider.html');
+	?>
+	</div>
+
+
+<div id='login'>
+
+	<h3>Login</h3>
+	<form class='horizontal-form' role='form'>
+		<div class="form-group">
+			<input type='text' name='user' class="form-control" id='user' placeholder="Username/e-mail" >
+			<input type='password' name='pass' class="form-control" id='pass' placeholder="Senha">
+			<input type="submit" class="btn btn-success" class="submit" text="Logar">
+		</div>
+	</form>
 </div>
 
-<form action="post">
-	<input type="text" name="user">
-	<input type="text" name="pass">
-</form>
+</div>
+
+
+
+
+
+
 
 </body>
+</html>
