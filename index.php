3383a49170bbc6ab0f4a405828b13d14d4b42d7d
celomamp <marcelo.pinheiro96@gmail.com>
2014-01-28 00:19:35 -0200

    organizacao de codigo

--- index.php
+++ index.php
@@ -36,31 +36,9 @@
 
 </nav>
 
-<h1 class="sub">Venha Louvar Conosco!</h1>
-<div class="container">
-	
-	<div class="slide">
-	<?php
-		require('slider.html');
-	?>
-	</div>
-
-
-<div id='login'>
-
-	<h3>Login</h3>
-	<form class='horizontal-form' role='form'>
-		<div class="form-group">
-			<input type='text' name='user' class="form-control" id='user' placeholder="Username/e-mail" >
-			<input type='password' name='pass' class="form-control" id='pass' placeholder="Senha">
-			<input type="submit" class="btn btn-success" class="submit" text="Logar">
-		</div>
-	</form>
-</div>
-
-</div>
-
-
+<?php
+	require('login.php');
+?>
 
 
 
